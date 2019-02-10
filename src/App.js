92553f7190df1bc1892954cc18034b19182f842a
skorns <skorns20@gmail.com>
2019-02-11 00:34:49 +0200

    add a comment - what is await, props, state and fetch

--- src/App.js
+++ src/App.js
@@ -12,6 +12,15 @@ const API_KEY = "efac62f8f1224430f1db691d407df751";
 // React.Component - от сюда происходит наследование
 class App extends React.Component {
 
+    /* ---------- props vs state ----------        
+        props (сокращение от «properties») и state - оба обычные объекты JavaScript. 
+        Хотя каждый из них содержат информацию, которая влияет на результат отрисовки компонента, 
+        между ними имеется важное отличие: 
+            - props передается компоненту (аналогично параметрам функции).
+            - state управляется внутри компонента (подобно переменным, объявленным внутри функции).
+        props внешние и не контролируются самим компонентом.
+        state приватен, определяется внутри компонента и доступен только из компонента.
+    */
     // переменные, которые будут сюда попадать после запроса и храниться (их можно передавать в другие компоненты)
     state = {
         temp: undefined,
@@ -28,15 +37,26 @@ class App extends React.Component {
         const city = e.target.elements.city.value;
 
         if(city) {
+            /* ---------- await ----------
+                Оператор await заставляет функцию, объявленную с использованием оператора async, 
+                ждать выполнения Promise и продолжать выполнение после возвращения Promise значения.
+                Объект Promise (обещание) используется для отложенных и асинхронных вычислений
+            */
+           /* ---------- fetch ----------
+                fetch - осуществления запросов к серверу:
+           */
             const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
             const data = await api_url.json();
-            console.log('%cГород: '+data.name, 'font-size:30px;'); // обьект data
+            // из обьекта data вытягиваем .name
+            console.log('%cГород: '+data.name, 'font-size:30px;');
 
             var sunset = data.sys.sunset;
             var date = new Date();
             date.setTime(sunset);
             var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
+            // this - обращаемся к этому класу - App
+            // Сэтим в стейт App то, что пришло в модельке
             this.setState({
                 temp: data.main.temp,
                 city: data.name,
@@ -44,7 +64,6 @@ class App extends React.Component {
                 sunrise: data.sys.sunrise,
                 sunset: sunset_date,
                 error: ""
-    
             });
         }
     }
@@ -57,7 +76,7 @@ class App extends React.Component {
                 <Info />
 
                 {/* Передаем переменную weatherMethod в компонент Form */}
-                {/* this - обращаемся к этому класу App */}
+                {/* this - обращаемся к этому класу - App */}
                 {/* gettingWeather - берем этот метод */}
                 <Form weatherMethod={this.gettingWeather} />
 
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,20 +1,37 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 class Weather extends React.Component {
     render() {
         return (
             <div>
-                { this.props.city && // проверяем не пусто ли в пропсах
+                { this.props.city && ( // проверяем не пусто ли в пропсах
                     <div>
                         <p>Местоположение: {this.props.city}, {this.props.country}</p>
                         <p>Температура: {this.props.temp}</p>
                         <p>Восход солнца: {this.props.sunrise}</p>
                         <p>Заход солнца: {this.props.sunset}</p>
                     </div>
-                }
+                )}
             </div>
         );
     }
 }
+
+
+/* ---------- propTypes ----------
+    propTypes снизу можно и не писать
+    В процессе разработки, если какому-то компоненту
+    не будет передано нужное свойство или свойство 
+    иного типа, React создаст лог ошибки.
+    Плюс сразу видно, какие пропсы передаются компоненту.
+*/
+Weather.propTypes = {
+    city: PropTypes.string,
+    country: PropTypes.string,
+    temp: PropTypes.number,
+    sunrise: PropTypes.number,
+    sunset: PropTypes.string,
+};
 
 export default Weather;
